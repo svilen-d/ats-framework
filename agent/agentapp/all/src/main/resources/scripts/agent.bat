239d9d68b945411138a1e348d2f22b5d7006e5ab
svilen-d <svilen-d@users.noreply.github.com>
2025-09-29 18:08:28 +0300

    ATS Agent - add-opens for Win

--- agent/agentapp/all/src/main/resources/scripts/agent.bat
+++ agent/agentapp/all/src/main/resources/scripts/agent.bat
@@ -15,7 +15,7 @@
 ::Example added through CMD -> agent.bat start -java_opts "-Dprop1=abc"
 
 ::Example added through CMD -> agent.bat start -java_opts "-Dprop1=abc" -java_opts "-Dprop2=cba"
-@SET JAVA_OPTS=%JAVA_OPTS%
+@SET JAVA_OPTS="%JAVA_OPTS% --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.management/javax.management.openmbean=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED"
 
 :: the Agent port
 @SET PORT=8089
